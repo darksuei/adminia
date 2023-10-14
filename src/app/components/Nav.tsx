12779196fa23f1b94aef14c8c5fe3f4d73c3e277
darksuei <chrisraphael576@gmail.com>
2023-10-14 21:30:05 +0100

    (feat): Add LogOut

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="w-full h-20 flex flex-row justify-around items-center gap-10">
+    <nav className="w-full h-20 flex flex-row justify-around items-center gap-10 mb-2">
       <h1 className="text-xl font-bold cursor-pointer">Adminia</h1>
       <ul className="flex flex-row gap-10">
         <li className="cursor-pointer text-sm hover:text-red-400">
@@ -10,6 +10,7 @@ export default function Nav() {
         </li>
         <li className="cursor-pointer text-sm hover:text-red-400">Account</li>
         <li className="cursor-pointer text-sm hover:text-red-400">Settings</li>
+        <li className="cursor-pointer text-sm hover:text-red-400">Log Out</li>
       </ul>
     </nav>
   );
