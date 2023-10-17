15a615b8478cabfd8d267e7e4313a1e435a25ba9
darksuei <chrisraphael576@gmail.com>
2023-10-17 10:55:36 +0100

    chore: fetch dynamic data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+
 import Nav from "../components/Nav";
 import { dummy_data } from "@/data";
+import useSWR from "swr";
+import { Suspense } from "react";
+import Cookies from "js-cookie";
 
 export default function Dashboard() {
+  const token = Cookies.get("token");
+
+  const fetcher = (url: string, token: string) =>
+    fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    `${process.env.NEXT_PUBLIC_SERVER}/get_db`,
+    (url) => fetcher(url, token!)
+  );
+  console.log(data);
+
   return (
     <main>
       <Nav />
@@ -10,16 +30,18 @@ export default function Dashboard() {
         <p className="text-gray-400 text-sm">
           Create a new project or manage existing projects
         </p>
-        <ol className="flex flex-col gap-3">
-          {dummy_data.map((data, idx) => (
-            <li
-              className="flex flex-row gap-2 w-10/12 mx-auto bg-white rounded-lg text-black h-14 items-center justify-between px-4 cursor-pointer hover:bg-gray-200"
-              key={idx}
-            >
-              {data.name}
-            </li>
-          ))}
-        </ol>
+        <Suspense fallback={<div>Loading..</div>}>
+          <ol className="flex flex-col gap-3">
+            {dummy_data.map((data, idx) => (
+              <li
+                className="flex flex-row gap-2 w-10/12 mx-auto bg-white rounded-lg text-black h-14 items-center justify-between px-4 cursor-pointer hover:bg-gray-200"
+                key={idx}
+              >
+                {data.name}
+              </li>
+            ))}
+          </ol>
+        </Suspense>
       </section>
     </main>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,13 @@ import Cookies from "js-cookie";
 export function storeInCookie(key: string, value: string) {
   Cookies.set(key, value, { expires: 1 });
 }
+
+export function mapData(data: any) {
+  const mappedData = new Map();
+  const keys = Object.keys(data);
+  keys.forEach((key) => {
+    mappedData.set(key, data[key]);
+  });
+  console.log(mappedData);
+  return mappedData;
+}
